0572f9510e47ce6ae5a86d9501b48d2ac87b6157
ssukhawani <ssukhawani@gmail.com>
2024-07-08 23:40:17 +0530

    Form popup

---
+++ components/PopupForm.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+
+const PopupForm = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://tally.so/widgets/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    window.TallyConfig = {
+      formId: "meEBxo",
+      popup: {
+        width: 460,
+        emoji: {
+          text: "👋",
+          animation: "wave",
+        },
+        autoClose: 10000,
+        doNotShowAfterSubmit: true,
+      },
+    };
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null;
+};
+
+export default PopupForm;
--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ import { QueryClientProvider, Hydrate } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { queryClient } from "../react-query/queryClient";
 import MaintenancePage from "../components/MaintenancePage";
+import PopupForm from "../components/PopupForm";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -55,6 +56,7 @@ function MyApp({ Component, pageProps }) {
             }}
             limit={3}
           />
+          <PopupForm />
         </>
       )}
     </>
